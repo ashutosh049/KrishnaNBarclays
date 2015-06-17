6f334d7e027a4c2bdd37ff6759fd3ae9c65ba508
karprix <karprix@outlook.com>
2015-06-17 23:22:31 +0530

    Create ConveyorSystem.java

---
+++ ConveyorSystem.java
@@ -0,0 +1,18 @@
+public ConveyorSystem implements ConveyorInterface
+{
+
+
+}
+
+
+//Utility classes
+class BagsList
+{
+  private ArrayList<String,String,String> bagsList = new ArrayList<String bagNumber,String entryPoint,String flightID>();
+    public ArrayList populateBagList()
+    {
+      bagsList.add("0001","Concourse_A_Ticketing","UA12");
+      bagsList.add("0002","A5","UA17");   
+    }
+
+}
