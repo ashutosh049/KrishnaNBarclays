746dc3144e34062e1ad7a10c8378f13870d9d989
karprix <karprix@outlook.com>
2015-06-29 01:16:00 +0530

    Delete ConveyorInterface.java

--- ConveyorInterface.java
+++
@@ -1,7 +0,0 @@
-public interface ConveyorInterface
-{
-  public int addTravelTimeBtwnNodes(String node1, String node2,int time);
-  public void addDepartureList(int flightID, String flightGate, String destination,String flightTime);
-  public void addBagList(String bagNumber, String entryPoint,String flightID);
-  public int calculateTotalTravelTime();
-}
